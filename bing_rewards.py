f3f76115fe45267d0609c6532dbeb988cf050ba6
Dave Hand <davehand@users.noreply.github.com>
2017-03-05 21:40:30 -0800

    Added redundancy for selenium failures

--- bing_rewards.py
+++ bing_rewards.py
@@ -12,83 +12,104 @@ from selenium.webdriver.support import expected_conditions as EC
 
 USERNAME = os.getenv('MICROSOFT_USERNAME')
 PASSWORD = os.getenv('MICROSOFT_PASSWORD')
+ALLOWED_WEB_SEARCHES = 30
+ALLOWED_MOBILE_SEARCHES = 20
 
-def bing_search_web(words):
-    driver = webdriver.Chrome('./chromedriver')
-    wait = WebDriverWait(driver, 10)
+def bing_search_web():
+    web_searches = 0
+    while web_searches < ALLOWED_WEB_SEARCHES:
+        try:
+            driver = webdriver.Chrome('./chromedriver')
+            wait = WebDriverWait(driver, 10)
 
-    driver.get('https://www.bing.com/')
-    time.sleep(1)
+            driver.get('https://www.bing.com/')
+            time.sleep(1)
 
-    #sign in
-    wait.until(EC.presence_of_element_located((By.ID, 'id_s')))
-    driver.find_element_by_id('id_s').click()
+            #sign in
+            wait.until(EC.presence_of_element_located((By.ID, 'id_s')))
+            driver.find_element_by_id('id_s').click()
 
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'id_name')))
-    driver.find_element_by_class_name('id_name').click()
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'id_name')))
+            driver.find_element_by_class_name('id_name').click()
 
-    wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
-    wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
-    driver.find_element_by_id('i0116').send_keys(USERNAME)
-    driver.find_element_by_id('idSIButton9').click()
+            wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
+            wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
+            driver.find_element_by_id('i0116').send_keys(USERNAME)
+            driver.find_element_by_id('idSIButton9').click()
 
-    wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
-    wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
-    driver.find_element_by_id('i0118').send_keys(PASSWORD)
-    driver.find_element_by_id('idSIButton9').click()
+            wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
+            wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
+            driver.find_element_by_id('i0118').send_keys(PASSWORD)
+            driver.find_element_by_id('idSIButton9').click()
 
-    time.sleep(1) #will redirect back to Bing
+            time.sleep(1) #will redirect back to Bing
 
-    for i in xrange(30):
-        wait.until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
-        driver.find_element_by_id('sb_form_q').clear()
-        driver.find_element_by_id('sb_form_q').send_keys(random.choice(words), Keys.RETURN)
-        time.sleep(5)
+            num_searches_remaining = ALLOWED_WEB_SEARCHES - web_searches
+            for i in xrange(num_searches_remaining):
+                wait.until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
+                driver.find_element_by_id('sb_form_q').clear()
+                driver.find_element_by_id('sb_form_q').send_keys(generate_search(), Keys.RETURN)
+                web_searches += 1
+                time.sleep(5)
+        except Exception:
+            print "Caught exception. Continuing"
+        finally:
+            driver.close()
 
-    driver.close()
-
-
-def bing_search_mobile(words):
+def bing_search_mobile():
     opts = Options()
     opts.add_argument('user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4')
 
-    driver = webdriver.Chrome('./chromedriver', chrome_options=opts)
-    wait = WebDriverWait(driver, 10)
+    mobile_searches = 0
+    while mobile_searches < ALLOWED_MOBILE_SEARCHES:
+        try:
+            driver = webdriver.Chrome('./chromedriver', chrome_options=opts)
+            wait = WebDriverWait(driver, 10)
 
-    driver.get('https://www.bing.com/')
-    time.sleep(1)
+            driver.get('https://www.bing.com/')
+            time.sleep(1)
 
-    #sign in
-    wait.until(EC.presence_of_element_located((By.ID, 'mHamburger')))
-    driver.find_element_by_id('mHamburger').click()
+            #sign in
+            wait.until(EC.presence_of_element_located((By.ID, 'mHamburger')))
+            driver.find_element_by_id('mHamburger').click()
 
-    time.sleep(2) #needs to wait a bit longer here for DOM to update
-    wait.until(EC.presence_of_element_located((By.ID, 'HBSignIn')))
-    driver.find_element_by_id('HBSignIn').click()
+            time.sleep(2) #needs to wait a bit longer here for DOM to update
+            wait.until(EC.presence_of_element_located((By.ID, 'HBSignIn')))
+            driver.find_element_by_id('HBSignIn').click()
 
-    wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
-    wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
-    driver.find_element_by_id('i0116').send_keys(USERNAME)
-    driver.find_element_by_id('idSIButton9').click()
+            wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
+            wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
+            driver.find_element_by_id('i0116').send_keys(USERNAME)
+            driver.find_element_by_id('idSIButton9').click()
 
-    wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
-    wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
-    driver.find_element_by_id('i0118').send_keys(PASSWORD)
-    driver.find_element_by_id('idSIButton9').click()
+            wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
+            wait.until(EC.presence_of_element_located((By.ID, 'idSIButton9')))
+            driver.find_element_by_id('i0118').send_keys(PASSWORD)
+            driver.find_element_by_id('idSIButton9').click()
 
-    time.sleep(1) #will redirect back to Bing
+            time.sleep(1) #will redirect back to Bing
 
-    for i in xrange(20):
-        wait.until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
-        driver.find_element_by_id('sb_form_q').clear()
-        driver.find_element_by_id('sb_form_q').send_keys(random.choice(words), Keys.RETURN)
-        time.sleep(5)
+            num_searches_remaining = ALLOWED_MOBILE_SEARCHES - mobile_searches
+            for i in xrange(num_searches_remaining):
+                wait.until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
+                driver.find_element_by_id('sb_form_q').clear()
+                driver.find_element_by_id('sb_form_q').send_keys(generate_search(), Keys.RETURN)
+                mobile_searches += 1
+                time.sleep(5)
+        except Exception:
+            print "Caught exception. Continuing"
+        finally:
+            driver.close()
 
-    driver.close()
-
+def generate_search():
+    num_words = random.randint(1,5)
+    search_string = ""
+    for i in xrange(num_words):
+        search_string += random.choice(words) + " "
+    return search_string.strip()
 
 with open('words.txt') as f:
     words = f.readlines()
 
-bing_search_web(words)
-bing_search_mobile(words)
+bing_search_web()
+bing_search_mobile()
